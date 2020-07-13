22b1504e0391557691a3981b191e86fa60410b9e
Avineak <ivawudal@gmail.com>
2020-07-13 15:01:05 +0545

    Update git links

--- src/Lists/Projects.js
+++ src/Lists/Projects.js
@@ -4,7 +4,7 @@ const Projects = [
     body:
       "In this project, Vue is implemented as frontend frameword and minimax algorithm for the AI capability. It is an unbeatable AI that looks for the best winning path.",
     icons: ["js", "vue", "html", "css"],
-    link: "http://google.com",
+    link: "https://github.com/Avineak/tic-tac",
     picture: "./Assets/vue.png",
   },
   {
@@ -12,7 +12,7 @@ const Projects = [
     body:
       "The game is created for people struggling typing fast. It introduce interesting game of tom-jerry chase to motivate and challenge typing skills",
     icons: ["c", "star"],
-    link: "http://google.com",
+    link: "https://github.com/Avineak/typing-master",
     picture: "./Assets/sfml.png",
   },
   {
@@ -20,7 +20,7 @@ const Projects = [
     body:
       "An app design that targets beginner rider as well as experienced rider. It has guides for the begineer riders with traffic rules and maps for the experienced riders",
     icons: ["js", "react", "css", "html"],
-    link: "http://google.com",
+    link: "https://github.com/Avineak/rider-book",
     picture: "./Assets/rider.png",
   },
   {
@@ -28,7 +28,7 @@ const Projects = [
     body:
       "It is simple gender detecting app that uses computer front camera to capture photos, then perform analysis and results out the gender of a person",
     icons: ["python"],
-    link: "http://google.com",
+    link: "https://github.com/Avineak/gender-detection",
     picture: "./Assets/tensor.png",
   },
 ];
